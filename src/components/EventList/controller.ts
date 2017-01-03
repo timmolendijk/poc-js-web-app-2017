8f433fa4828312f5f55e4d25a98682ef17e15ab4
Tim Molendijk <tim@timmolendijk.nl>
2017-01-03 19:03:01 +0100

    Abstract all the typical field stuff away in a set of composable field decorators: - field; - observable; - object(s) [TBD]

--- src/components/EventList/controller.ts
+++ src/components/EventList/controller.ts
@@ -1,5 +1,4 @@
-import { observable, computed, asReference } from 'mobx';
-import { field, getContainer, getUntrackedField, setUntrackedField } from 'state';
+import { oldField, field, observable, getContainer, getUntrackedField, setUntrackedField } from 'state';
 import { reportOnError } from 'error';
 import { ITransport, isTransportError } from 'transport';
 import { Event } from 'models';
@@ -8,67 +7,20 @@ import { pick } from 'lodash';
 export default class EventListController {
 
   constructor(data?) {
-    Object.assign(this, {
-      events: [],
-      loading: false
-    }, pick(data, ['events']));
-
     setTimeout(() => reportOnError(this.load()));
   }
 
-  private _observable_events;
-  @field(
-    (refs, hydrate) => refs.map(hydrate),
-    (events, dehydrate) => events.map(dehydrate),
-    function () {
-      const container = getContainer(this);
-      this._observable_events = observable(asReference(undefined));
-      this._observable_events.set(container.getField(this, 'events'));
-      container.store.subscribe(() =>
-        this._observable_events.set(container.getField(this, 'events'))
-      );
+  @observable(field)({
+    dataToValue(refs, hydrate) {
+      return refs.map(hydrate);
+    },
+    valueToData(events, dehydrate) {
+      return events.map(dehydrate);
     }
-  )
-  @computed get events(): ReadonlyArray<Event> {
-    const container = getContainer(this);
-    if (container)
-      return this._observable_events.get().map(ref => container.getObject(ref));
-    else
-      return getUntrackedField(this, 'events');
-  }
-  set events(value: ReadonlyArray<Event>) {
-    const container = getContainer(this);
-    if (container)
-      container.setField(this, 'events', value.map(object => container.createReference(object)));
-    else
-      setUntrackedField(this, 'events', value);
-  }
+  }) events: ReadonlyArray<Event> = [];
 
-  private _observable_loading;
-  @field(undefined, undefined, function () {
-    const container = getContainer(this);
-    this._observable_loading = observable(asReference(undefined));
-    this._observable_loading.set(container.getField(this, 'loading'));
-    container.store.subscribe(() =>
-      this._observable_loading.set(container.getField(this, 'loading'))
-    );
-  })
-  get loading(): boolean {
-    const container = getContainer(this);
-    if (container)
-      return this._observable_loading.get();
-    else
-      return getUntrackedField(this, 'loading');
-  }
-  set loading(value: boolean) {
-    const container = getContainer(this);
-    if (container)
-      container.setField(this, 'loading', value);
-    else
-      setUntrackedField(this, 'loading', value);
-  }
+  @observable(field)() loading: boolean = false;
 
-  // TODO(tim): Caller should wrap its call to `load` in `reportOnError`.
   async load() {
     this.loading = true;
 
@@ -78,8 +30,6 @@ export default class EventListController {
     try {
       instances = await page;
     } catch (err) {
-      // TODO(tim)
-      console.log(err);
       if (isTransportError(err))
         return;
       throw err;
