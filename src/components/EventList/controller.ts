7b511a69e1471be34d06beacd2ef9d0a08b55adc
Tim Molendijk <tim@timmolendijk.nl>
2017-01-04 17:53:29 +0100

    Re-enable server-side rendering with awaiting data.
    Implement clunky but working lazy loading construct in field decorator.

--- src/components/EventList/controller.ts
+++ src/components/EventList/controller.ts
@@ -1,23 +1,27 @@
+import { IAwaitable } from 'await';
 import { action, field, objects, observable } from 'state';
 import { reportOnError } from 'error';
 import { ITransport, isTransportError } from 'transport';
 import { Event } from 'models';
 
-export default class EventListController {
+export default class EventListController implements IAwaitable {
 
-  constructor() {
-    // TODO(tim): Move to some "lazy load" hook on `events` field.
-    setTimeout(() => reportOnError(this.load()));
-  }
-
-  @observable(objects(field))() events: ReadonlyArray<Event> = [];
+  @observable(objects(field))({
+    // TODO(tim): This feels like an ugly silly lazy loading construct.
+    onGet(value) {
+      if (value && value.length > 0 || this.loading)
+        return;
+      reportOnError(this.load());
+    }
+  }) events: ReadonlyArray<Event> = [];
   @observable(field)() loading: boolean = false;
+
+  private readonly transport: ITransport<Event> & IAwaitable = new Event.Transport;
 
   async load() {
     this.startLoad();
 
-    const transport: ITransport<Event> = new Event.Transport;
-    const page = transport.list();
+    const page = this.transport.list();
     let instances;
     try {
       instances = await page;
@@ -39,6 +43,10 @@ export default class EventListController {
   endLoad(events) {
     this.events = events;
     this.loading = false;
+  }
+
+  get await() {
+    return this.transport.await;
   }
 
 }
