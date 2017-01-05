bf7373bb106fb9d21ab318db2f9cac1d5bd347d5
Tim Molendijk <tim@timmolendijk.nl>
2017-01-05 16:32:28 +0100

    Helloooo there Scoopy!
    The rewrite allowing to put fields pretty much anywhere is coming along!

--- src/components/ScoopyTest.tsx
+++ src/components/ScoopyTest.tsx
@@ -3,8 +3,45 @@ import { Provider } from 'react-redux';
 import { computed } from 'mobx';
 import { observer } from 'mobx-react';
 import { NS, Container, observable } from 'scoopy';
+import { reportOnError } from 'error';
+import { isTransportError } from 'transport';
+import { Event } from 'models';
+
+class Controller {
+
+  @observable private events: ReadonlyArray<Event>;
+  @observable loading: boolean = false;
+
+  getEvents(fallback = null) {
+    if (!this.events && !this.loading)
+      reportOnError(this.load());
+
+    return this.events || fallback;
+  }
+
+  private async load() {
+    this.loading = true;
+
+    const page = Event.transport.list();
+    let instances;
+    try {
+      instances = await page;
+    } catch (err) {
+      if (isTransportError(err))
+        return;
+      throw err;
+    }
+
+    this.events = instances;
+    this.loading = false;
+  }
+
+}
 
 @observer class ScoopyComponent extends React.Component<{}, {}> {
+  
+  // TODO(tim): `observable` doesn't make much sense here.
+  @observable private readonly controller = new Controller();
 
   @observable private cheer: string = "joepie";
 
@@ -17,7 +54,19 @@ import { NS, Container, observable } from 'scoopy';
       <h1>{this.loudCheer} SCOOPY!</h1>
       <button type="button" onClick={() => this.cheer = "yay"}>yay</button>
       <button type="button" onClick={() => this.cheer = "aitait"}>aitait</button>
+      {this.renderEvents()}
     </div>;
+  }
+
+  private renderEvents() {
+    if (this.controller.loading)
+      return <p>LOADING…</p>;
+    
+    return <ul>
+      {this.controller.getEvents([]).map(event =>
+        <li key={event.id}>{event.name}</li>
+      )}
+    </ul>;
   }
 
 }
